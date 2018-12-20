53e88d8b37e45c2af06e9050de8e0e0f98f4e833
Francisco Ribeiro <franciscoribeiro24@gmail.com>
2018-12-20 17:56:32 +0000

    Integração defeituosa de Flask-Admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+admin = Admin(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    email = db.Column(db.String(64))
+    phone = db.Column(db.Integer())
+
+admin.add_view(ModelView(Contact, db.session))
 
 from app import routes
--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired("Please enter your name")])
