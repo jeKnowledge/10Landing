f5e43e2a63f14f12dde8a71fe8de8cab9d573fa8
Tiago Martins <tiagomartins282@gmail.com>
2019-01-11 20:13:45 +0000

    move models to a single file and fix migrations folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,16 +10,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 admin = Admin(app)
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32))
-    email = db.Column(db.String(64))
-    phone = db.Column(db.Integer())
-    zip = db.Column(db.Integer())
-
-    def __repr__(self):
-        return '<Contact {}>'.format(self.name)
-
-admin.add_view(ModelView(Contact, db.session))
-
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -3,6 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey123454'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'asurea_landing.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'asurea_landing.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
